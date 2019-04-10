c29976f13919d60c0846c11edcbde58d1acc37b4
jiangshipan <1320740751@qq.com>
2019-04-10 20:46:25 +0800

    提交更新

--- src/main/java/com/jsp/footmap/FootmapApplication.java
+++ src/main/java/com/jsp/footmap/FootmapApplication.java
@@ -3,8 +3,10 @@ package com.jsp.footmap;
 import com.jsp.footmap.controller.ThreadController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 
-import java.net.URL;
+import javax.servlet.MultipartConfigElement;
 
 
 @SpringBootApplication
@@ -14,4 +16,16 @@ public class FootmapApplication {
 		SpringApplication.run(FootmapApplication.class, args);
 	}
 
+	/**
+	 * 配置上传文件大小
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//单个数据大小
+		factory.setMaxFileSize("1024KB");
+		factory.setMaxRequestSize("1024KB");
+		return factory.createMultipartConfig();
+	}
 }
